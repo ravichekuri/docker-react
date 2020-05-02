f21c994db7a750795ae8b4d6436c1e80abcfb2d3
ravichekuri <ravichekuridevops@gmail.com>
2020-05-02 09:59:02 +0530

    docker learning

---
+++ build/precache-manifest.f13ff6cf20357dd5ba68b8126087b49e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd64d20b435b712d5057d85184db9ed6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23dffdb1a370fbb28612",
+    "url": "/static/css/main.4b738920.chunk.css"
+  },
+  {
+    "revision": "99189bbfa8480956ead4",
+    "url": "/static/js/2.34172111.chunk.js"
+  },
+  {
+    "revision": "5a6f9c99bb61555b2e83ba8eacabf86f",
+    "url": "/static/js/2.34172111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23dffdb1a370fbb28612",
+    "url": "/static/js/main.26feb8c5.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
